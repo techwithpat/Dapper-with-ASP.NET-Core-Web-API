bc1149ba9f7ff2f6883c959a0e18cb48182bef4d
patrick <patrick@makanda.io>
2021-08-04 22:12:51 +0200

    Add project files.

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public Task<Book> Create(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
