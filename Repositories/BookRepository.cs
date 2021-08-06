a2d7ea0e19b1212221138446c6075639044436b5
patrick <patrick@makanda.io>
2021-08-06 13:45:14 +0200

    wrap up

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -9,23 +9,25 @@ namespace BookAPI.Repositories
 {
     public class BookRepository : IBookRepository
     {
-        private readonly IConfiguration _configuration;
-        private string _connectionString;
+        private readonly string _connectionString;
 
         public BookRepository(IConfiguration configuration)
         {
-            _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("Default");
+            _connectionString = configuration.GetConnectionString("Default");
         }
 
         public async Task<Book> Create(Book book)
         {
-            var sql = @"INSERT INTO Books (Title, Author, Description) VALUES (@Title, @Author, @Description)";
+            var sqlQuery = @"INSERT INTO Books (Title, Author, Description) VALUES (@Title, @Author, @Description)";
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                _ = await connection.ExecuteAsync(sql, new { book.Title, book.Author, book.Description })
-                    .ConfigureAwait(false);
+                await connection.ExecuteAsync(sqlQuery, new
+                {
+                    book.Title,
+                    book.Author,
+                    book.Description
+                });
 
                 return book;
             }
@@ -33,46 +35,72 @@ namespace BookAPI.Repositories
 
         public async Task<IEnumerable<Book>> Get()
         {
-            var sql = "SELECT * FROM Books";
+            var sqlQuery = "SELECT * FROM Books";
+
+            //using (var connection = new SqlConnection(_connectionString))
+            //{
+            //    var books = await connection.QueryAsync<Book>(sql);
+            //    return books;
+            //}  
+
+            var books = new List<Book>();
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                var books = await connection.QueryAsync<Book>(sql).ConfigureAwait(false);
-                return books;
+                using (var sqlCommand = new SqlCommand(sqlQuery, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataReader reader = await sqlCommand.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            books.Add(new Book
+                            {
+                                Id = reader.GetInt32(0),
+                                Title = reader.GetString(1),
+                                Author = reader.GetString(2),
+                                Description = reader.GetString(3)
+                            });
+                        }
+                    }
+                }
             }
+
+            return books;
         }
 
         public async Task<Book> Get(int id)
         {
-            var sql = "SELECT * FROM Books WHERE Id = @BookId";
+            var sqlQuery = "SELECT * FROM Books WHERE Id=@BookId";
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                var book = await connection.QueryFirstOrDefaultAsync<Book>(sql, new { BookId = id })
-                                           .ConfigureAwait(false);
-                return book;
+                return await connection.QueryFirstOrDefaultAsync<Book>(sqlQuery, new { BookId = id });                
             }
         }
 
         public async Task Update(Book book)
         {
-            var sql = "UPDATE Books SET Title='@Title', Author=@Author, Description=@Description WHERE Id = @Id;";
+            var sqlQuery = "UPDATE Books SET Title=@Title, Author=@Author, Description=@Description WHERE Id = @Id;";
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                _ = await connection.ExecuteAsync(sql, new {book.Id, book.Title, book.Author, book.Description})
-                    .ConfigureAwait(false);
+                await connection.ExecuteAsync(sqlQuery, new {
+                    book.Id, 
+                    book.Title, 
+                    book.Author, 
+                    book.Description});
             }
         }
 
         public async Task Delete(int id)
         {
-            var sql = "DELETE from Books WHERE Id = @Id;";
+            var sqlQuery = "DELETE from Books WHERE Id=@Id;";
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                _ = await connection.ExecuteAsync(sql, new { Id = id })
-                    .ConfigureAwait(false);
+                await connection.ExecuteAsync(sqlQuery, new { Id = id });
             }
         }       
     }
