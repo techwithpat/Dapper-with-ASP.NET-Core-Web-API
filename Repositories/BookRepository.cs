f804a347e120a3a6a9ab588440cf3d72da3ae4a5
patrick <patrick@makanda.io>
2021-08-06 13:53:26 +0200

    init

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,107 +1,34 @@
 ﻿using BookAPI.Models;
-using Dapper;
-using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace BookAPI.Repositories
 {
     public class BookRepository : IBookRepository
     {
-        private readonly string _connectionString;
-
-        public BookRepository(IConfiguration configuration)
+        public Task<IEnumerable<Book>> Get()
         {
-            _connectionString = configuration.GetConnectionString("Default");
+            throw new System.NotImplementedException();
         }
 
-        public async Task<Book> Create(Book book)
+        public Task<Book> Get(int id)
         {
-            var sqlQuery = @"INSERT INTO Books (Title, Author, Description) VALUES (@Title, @Author, @Description)";
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                await connection.ExecuteAsync(sqlQuery, new
-                {
-                    book.Title,
-                    book.Author,
-                    book.Description
-                });
-
-                return book;
-            }
+            throw new System.NotImplementedException();
         }
 
-        public async Task<IEnumerable<Book>> Get()
+        public Task<Book> Create(Book book)
         {
-            var sqlQuery = "SELECT * FROM Books";
-
-            //using (var connection = new SqlConnection(_connectionString))
-            //{
-            //    var books = await connection.QueryAsync<Book>(sql);
-            //    return books;
-            //}  
-
-            var books = new List<Book>();
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                using (var sqlCommand = new SqlCommand(sqlQuery, connection))
-                {
-                    connection.Open();
-
-                    using (SqlDataReader reader = await sqlCommand.ExecuteReaderAsync())
-                    {
-                        while (reader.Read())
-                        {
-                            books.Add(new Book
-                            {
-                                Id = reader.GetInt32(0),
-                                Title = reader.GetString(1),
-                                Author = reader.GetString(2),
-                                Description = reader.GetString(3)
-                            });
-                        }
-                    }
-                }
-            }
-
-            return books;
+            throw new System.NotImplementedException();
         }
 
-        public async Task<Book> Get(int id)
+        public Task Update(Book book)
         {
-            var sqlQuery = "SELECT * FROM Books WHERE Id=@BookId";
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                return await connection.QueryFirstOrDefaultAsync<Book>(sqlQuery, new { BookId = id });                
-            }
+            throw new System.NotImplementedException();
         }
 
-        public async Task Update(Book book)
+        public Task Delete(int id)
         {
-            var sqlQuery = "UPDATE Books SET Title=@Title, Author=@Author, Description=@Description WHERE Id = @Id;";
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                await connection.ExecuteAsync(sqlQuery, new {
-                    book.Id, 
-                    book.Title, 
-                    book.Author, 
-                    book.Description});
-            }
+            throw new System.NotImplementedException();
         }
-
-        public async Task Delete(int id)
-        {
-            var sqlQuery = "DELETE from Books WHERE Id=@Id;";
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                await connection.ExecuteAsync(sqlQuery, new { Id = id });
-            }
-        }       
     }
 }
